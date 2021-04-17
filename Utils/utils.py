790dc88328622fce43a80bbcef31c4c925da02ff
DavidYafe <david.yafe@gmail.com>
2021-04-17 17:00:06 +0300

    Add funcs

--- Utils/utils.py
+++ Utils/utils.py
@@ -5,35 +5,56 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import os
 #
 
-def get_yaml_path():
-    if "DavidYafe" in os.getcwd():
-        path =  "C:\\Users\DavidYafe\Desktop\MyCryptoAPI\Auth\conf.yaml"
-    else:
-        path = "C:\\Users\\1\PycharmProjects\Rater\Auth\conf.yaml"
-    return path
+
+def get_file_path(file_id):
+    # 1 = conf.yaml
+    # 2 = data.json
+    elad = "C:\\Users\\1\PycharmProjects\Rater\\"
+    david = "C:\\Users\DavidYafe\Desktop\MyCryptoAPI\\"
+    yaml_path = "Auth\conf.yaml"
+    json_path = "Files\Data.json"
+    if file_id ==1:
+        if "DavidYafe" in os.getcwd():
+            path =  david + yaml_path
+        else:
+            path = elad + yaml_path
+        return path
+    elif file_id ==2:
+        if "DavidYafe" in os.getcwd():
+            path =  david + json_path
+        else:
+            path = elad + json_path
+        return path
 
 
 
 
 
 def get_creds():
-    path = get_yaml_path()
+    path = get_file_path(1)
     yaml_file = open(path)
     parse_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
     return [parse_yaml["url"], parse_yaml["parameters"], parse_yaml["headers"]]
 
 
 
-# def get_data():
-#     session = Session()
-#     session.headers.update(headers)
-#     try:
-#       response = session.get(url, params=parameters)
-#       data = json.loads(response.text)
-#       print(data)
-#     except (ConnectionError, Timeout, TooManyRedirects) as e:
-#       print(e)
+def get_data():
+    url = get_creds()[0]
+    parameters = get_creds()[1]
+    headers = get_creds()[2]
+    session = Session()
+    session.headers.update(headers)
+    try:
+      response = session.get(url, params=parameters)
+      data = json.loads(response.text)
+      print(data)
+      path = get_file_path(2)
+      with open(path, 'w') as outfile:
+          json.dump(data, outfile)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+      print(e)
+    return data
 
 
 
-print(get_creds())
+print(get_file_path(2))
